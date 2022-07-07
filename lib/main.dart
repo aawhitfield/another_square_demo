c560c674859eb0114d05a84f035c7c83c6664cc4
Aaron Whitfield <aawhitfield@hotmail.com>
2022-07-06 22:02:16 -0700

    starts the process of creating an order

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:another_square/another_square.dart';
+import 'package:another_square/square_models.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,26 +30,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Center(
-      ),
+      body: Center(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           SquareClient? squareClient = SquareClient(
               applicationId: 'sandbox-sq0idb-8gHyay1byds6xd-dB0Iw6g',
               clientId: 'sandbox-sq0idb-8gHyay1byds6xd-dB0Iw6g',
-              clientSecret: 'sandbox-sq0csb-3m9WHFzmUYJOvX8BWGAi2Pdc7oQNiZT503OLXIPt8Z8'
-          );
+              clientSecret:
+                  'sandbox-sq0csb-3m9WHFzmUYJOvX8BWGAi2Pdc7oQNiZT503OLXIPt8Z8');
 
           await squareClient.initialize();
+
+          await squareClient.createOrder(
+              authToken:
+                  'EAAAEC5rEB5oloV1DvpspYLMClHBey32lMeI0if9DoN6k_kDutfmfQmKKylBF3ak',
+              request: request);
         },
         tooltip: 'Create Order',
         child: const Icon(Icons.add),
