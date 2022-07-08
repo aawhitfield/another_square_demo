10c63ea0dafa3e047cc16d9b9d376262a87c5036
Aaron Whitfield <aawhitfield@hotmail.com>
2022-07-07 22:44:22 -0700

    finished creating an order

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,50 @@ class _MyHomePageState extends State<MyHomePage> {
           await squareClient.createOrder(
               authToken:
                   'EAAAEC5rEB5oloV1DvpspYLMClHBey32lMeI0if9DoN6k_kDutfmfQmKKylBF3ak',
-              request: request);
+              request: CreateOrderRequest.fromJson(
+                  {
+                    "idempotency_key": "8193148c-9586-11e6-99f9-28cfe92138cf",
+                    "order": {
+                      "reference_id": "my-order-001",
+                      "location_id": "LW4FWFNE6JQ0Z",
+                      "line_items": [
+                        {
+                          "name": "New York Strip Steak",
+                          "quantity": "1",
+                          "base_price_money": {
+                            "amount": 1599,
+                            "currency": "USD"
+                          }
+                        },
+                      ],
+                      "taxes": [
+                        {
+                          "uid": "state-sales-tax",
+                          "name": "State Sales Tax",
+                          "percentage": "9",
+                          "scope": "ORDER"
+                        }
+                      ],
+                      "discounts": [
+                        {
+                          "uid": "labor-day-sale",
+                          "name": "Labor Day Sale",
+                          "percentage": "5",
+                          "scope": "ORDER"
+                        },
+                        {
+                          "uid": "one-dollar-off",
+                          "name": "Sale - \$1.00 off",
+                          "amount_money": {
+                            "amount": 100,
+                            "currency": "USD"
+                          },
+                          "scope": "LINE_ITEM"
+                        }
+                      ]
+                    }
+                  }
+              ));
         },
         tooltip: 'Create Order',
         child: const Icon(Icons.add),
